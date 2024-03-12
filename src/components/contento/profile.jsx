fb7967745e99675aeeb0b2cb540a3d94237a7d05
Edson Yamble <edsonyamble2@gmail.com>
2024-03-12 20:16:51 +0700

    first commit

---
+++ src/components/contento/profile.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+function Profile() {
+	return (
+		<div className='content'>
+			<div>
+				<img
+					src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'
+					alt='flower'
+				/>
+			</div>
+			<div>ava+ description</div>
+			<div>
+				my posts
+				<div>news posts</div>
+			</div>
+			<div>
+				<div>post 1</div>
+				<div>post 2</div>
+			</div>
+		</div>
+	)
+}
+export default Profile
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+const Navbar = () => {
+	return (
+		<nav className='nav'>
+			<div>
+				<a href='#s'>Profile</a>
+			</div>
+			<div>
+				<a href='#s'>Messages</a>
+			</div>
+			<div>
+				<a href='#s'>News</a>
+			</div>
+			<div>
+				<a href='#s'>Musics</a>
+			</div>
+			<div>
+				<a href='#s'>Settings</a>
+			</div>
+		</nav>
+	)
+}
+export default Navbar
